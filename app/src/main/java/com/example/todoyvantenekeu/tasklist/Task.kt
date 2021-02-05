2974261c40470c53eeee5f0e97dbf4b5c413ea5a
ttcyvan <tenekeucabrel@gmail.com>
2021-02-05 14:15:33 +0100

    evolution 2

---
+++ app/src/main/java/com/example/todoyvantenekeu/tasklist/Task.kt
@@ -0,0 +1,14 @@
+package com.example.todoyvantenekeu.tasklist
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+@kotlinx.serialization.Serializable
+
+data class Task(
+        @SerialName("id")
+        val id: String,
+        @SerialName("title")
+        val title: String,
+        @SerialName("description")
+        val description: String
+): java.io.Serializable
--- app/src/main/java/com/example/todoyvantenekeu/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/todoyvantenekeu/tasklist/TaskListFragment.kt
@@ -8,11 +8,17 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.example.todoyvantenekeu.R
+import java.util.*
 
 class TaskListFragment : Fragment(){
-    private val taskList = listOf("Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3")
+    //private val taskList = listOf("Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3","Task 1", "Task 2", "Task 3")
+    private val taskList = mutableListOf(
+            Task(id = "id_1", title = "Task 1", description = "description 1"),
+            Task(id = "id_2", title = "Task 2"),
+            Task(id = "id_3", title = "Task 3")
+    )
+    private val adapter = TaskListAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,22 +31,22 @@ class TaskListFragment : Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
-        //val floating_button = view.findViewById<RecyclerView>(R.id.floatingActionButton)
+        val floating_button = view.findViewById<RecyclerView>(R.id.add_task)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = TaskListAdapter(taskList)
-       /* floating_button.setOnClickListener {
+        floating_button.setOnClickListener {
             taskList.add(Task(id = UUID.randomUUID().toString(), title = "Task ${taskList + 1}"))
-            TaskListAdapter.submitList(taskList.toList())
-        }*/
+            adapter.submitList(taskList.toList())
+        }
     }
 }
 
-class TaskListAdapter(private val taskList: List<String>): RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>(){
+class TaskListAdapter(private val taskList: List<Task>): RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>(){
     inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(taskTitle: String) {
+        fun bind(taskTitle: List<Task>) {
             itemView.apply {
                 val tv = itemView.findViewById<TextView>(R.id.task_title)
-                tv.text = taskTitle
+                tv.text = taskTitle.toString()
             }
         }
     }
@@ -55,7 +61,7 @@ class TaskListAdapter(private val taskList: List<String>): RecyclerView.Adapter<
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        holder.bind(taskList[position])
+        holder.bind(taskList)
     }
 
 }
